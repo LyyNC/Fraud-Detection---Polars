0e7fbe51d28861ad42980fd98508647697ba40c1
LyyNC <linconmicee@hotmail.com>
2026-06-12 16:36:59 -0300

    Realizado até o passo 4 da atividade.

--- Nucleo-de-Inteligencia-Artificial-GE-T0/build_fraud.py
+++ Nucleo-de-Inteligencia-Artificial-GE-T0/build_fraud.py
@@ -3,6 +3,7 @@
 # - # - # - # - # -
 
 import polars as pl
+import numpy as np
 
 #Carregando os DataSet 'train_transaction' e 'train_identity' utilizando o pl.read_csv
 train_transaction = pl.read_csv("Fraud Detection/train_transaction.csv")
@@ -36,6 +37,39 @@ df = train_transaction.join(train_identity, on="TransactionID")
 
 print(df.head())
 
+#Verificando se há algum nulo na coluna TransactionID
 print(df["TransactionID"].null_count())
 
-print(df.columns)
+# - # - # - # - # -
+#Etapa 3º Seleção de colunas relevantes
+# - # - # - # - # -
+
+#Separando as colunas relevantes para ter apenas um dataframe com as devidas colunas relevantes utilizando o .selec
+col_relevantes = ["TransactionDT", "TransactionAmt", "ProductCD", "card1", "DeviceType", "isFraud"]
+df_relevantes = df.select(pl.col(col_relevantes))
+print(df_relevantes.head())
+
+# - # - # - # - # -
+#Etapa 4º Filtragem de fraudes
+# - # - # - # - # -
+
+#Filtrando os casos de fraudes (Fraudes = 1/ Não Fraude = 0) utilizando o .filter 
+qnt_fraude = df_relevantes.filter(
+    (pl.col("isFraud") == 1)).height
+
+print(f"Quantidade de Fraudes identificadas: {qnt_fraude}")
+#Calculando a proporção de casos de fraudes. 
+total_trans = df_relevantes["isFraud"].len()
+proporc_fraude = (qnt_fraude / total_trans) * 100
+print(f"Proporção de casos identificado como Fraude é de {proporc_fraude}%")
+
+isfraud = df_relevantes.filter(
+    (pl.col("isFraud") == 1))
+
+
+produto = (
+    df_relevantes.group_by("ProductCD")
+    .agg(pl.col("TransactionAmt").mean().alias("Média TransactionAmt"),
+         pl.col("isFraud").mean()))
+
+print(produto)
